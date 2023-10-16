9bb89d094a55c290625621146b106bce76b6182c
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2023-10-15 20:54:20 -0700

    All done just need to go live

--- src/index.js
+++ src/index.js
@@ -4,32 +4,33 @@ import fetchData from './fetchAPI';
 
 // alt+248 degree symbol
 
+let tempInC; let tempInF;
+let feelsInC; let feelsInF;
 
 
-//  Loading default data for Kamloops onload
 function DOMcontroller(data){
-    console.log('temp', data);
+    tempInC = data.current.temp_c;
+    tempInF = data.current.temp_f;
+    feelsInC = data.current.feelslike_c;
+    feelsInF = data.current.feelslike_f;
+    setTemps();
     const locationContainer = document.querySelector('.location-container');
-    const tempContainer = document.querySelector('.temp-container');
     const iconContainer = document.querySelector('.icon-container');
     const conditionContainer = document.querySelector('.weather-condition-container');
-    const feelsContainer = document.querySelector('.feels-like-container');
     const precContainer = document.querySelector('.precipitation-container');
     const humidContainer = document.querySelector('.humidity-container');
     const pressContainer = document.querySelector('.pressure-container');
-
-
-
-    tempContainer.textContent = `${data.current.temp_c} °C`;
     locationContainer.textContent = `${data.location.name}, ${data.location.region}`;
     iconContainer.src = data.current.condition.icon;    
     conditionContainer.textContent = data.current.condition.text;    
-    feelsContainer.textContent = `${data.current.temp_c} °C`;
     precContainer.textContent = `${data.current.precip_mm} mm`;
     humidContainer.textContent = `${data.current.humidity}`;
     pressContainer.textContent = `${data.current.pressure_mb} hPa`;
 
 }
+
+
+//  Loading default data for Kamloops onload
 window.onload = () =>{
     fetchData('Kamloops')
     .then( data => DOMcontroller(data))
@@ -51,4 +52,26 @@ inp.addEventListener("keypress", (e)=>{
 })
 
 
+//  Checkbox False = C and True = F
 
+const toggleSwitch = document.querySelector('.checkbox');
+
+toggleSwitch.addEventListener('click', () =>{
+    setTemps();
+});
+
+
+function setTemps(){
+    const tempContainer = document.querySelector('.temp-container');
+    const feelsContainer = document.querySelector('.feels-like-container');
+    if(!toggleSwitch.checked) 
+    {  
+        tempContainer.textContent = `${tempInC} °C`;
+        feelsContainer.textContent = `${feelsInC} °C`;
+    }
+    else 
+    {
+        tempContainer.textContent = `${tempInF} °F`;
+        feelsContainer.textContent = `${feelsInF} °F`;
+    }
+}
