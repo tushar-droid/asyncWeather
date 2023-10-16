78f01a5c19216178575c5f3143767d90865b5ced
tushar-droid <63186251+tushar-droid@users.noreply.github.com>
2023-10-15 20:04:49 -0700

    Pushed data in containers

--- src/index.js
+++ src/index.js
@@ -13,11 +13,22 @@ function DOMcontroller(data){
     const tempContainer = document.querySelector('.temp-container');
     const iconContainer = document.querySelector('.icon-container');
     const conditionContainer = document.querySelector('.weather-condition-container');
+    const feelsContainer = document.querySelector('.feels-like-container');
+    const precContainer = document.querySelector('.precipitation-container');
+    const humidContainer = document.querySelector('.humidity-container');
+    const pressContainer = document.querySelector('.pressure-container');
 
-    tempContainer.textContent = `${data.current.feelslike_c} °C`;
+
+
+    tempContainer.textContent = `${data.current.temp_c} °C`;
     locationContainer.textContent = `${data.location.name}, ${data.location.region}`;
     iconContainer.src = data.current.condition.icon;    
     conditionContainer.textContent = data.current.condition.text;    
+    feelsContainer.textContent = `${data.current.temp_c} °C`;
+    precContainer.textContent = `${data.current.precip_mm} mm`;
+    humidContainer.textContent = `${data.current.humidity}`;
+    pressContainer.textContent = `${data.current.pressure_mb} hPa`;
+
 }
 window.onload = () =>{
     fetchData('Kamloops')
